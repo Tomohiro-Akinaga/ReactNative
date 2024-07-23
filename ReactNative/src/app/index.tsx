f634a2ffb129a9c39b284794a27397ee82c4da21
Tomohiro-Akinaga <55231048+Tomohiro-Akinaga@users.noreply.github.com>
2024-07-23 19:06:47 +0900

    WIP

--- ReactNative/src/app/index.tsx
+++ ReactNative/src/app/index.tsx
@@ -1,59 +1,7 @@
-import { View, Text, StyleSheet } from "react-native";
+import { Redirect } from "expo-router";
 
 const Index = (): JSX.Element => {
-  return (
-    <View style={styles.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
-        </View>
-      </View>
-      <View>
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-      </View>
-
-      <View>
-        <Text>+</Text>
-      </View>
-    </View>
-  );
+  return <Redirect href="memo/list" />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default Index;
