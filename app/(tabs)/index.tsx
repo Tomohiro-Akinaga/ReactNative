090514b9bdfb39bbe985358fd04c29cef3cd6698
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-07-03 23:29:54 +0800

    feat: todo入力画面の実装

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,40 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Button, StyleSheet, Text, TextInput } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const [text, setText] = React.useState("");
+  const [todos, setTodos] = React.useState<string[]>([]);
+
+  const handleAddTodo = () => {
+    setTodos([...todos, text]);
+    setText("");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <TextInput style={styles.input} onChangeText={setText} value={text} />
+        {todos.map((v, i) => (
+          <Text key={i}>{v}</Text>
+        ))}
+        <Button title="Todoを追加" onPress={handleAddTodo} />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    height: "100%",
-  },
-  text: {
-    // スタイルを空にすることで、Viewの中央にTextが表示される
+  // container: {
+  //   display: "flex",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  //   height: "100%",
+  // },
+  // text: {},
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
