c609dab9c0f72baa83485bf8be219a7bf4de8ad3
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-07-08 22:41:02 +0800

    Update TodoItem.tsx

--- app/_components/TodoItem.tsx
+++ app/_components/TodoItem.tsx
@@ -1,6 +1,6 @@
 import { Checkbox } from "expo-checkbox";
-import React from "react";
-import { Button, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import React, { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from "react-native";
 
 type Props = {
   todo: {
@@ -13,10 +13,18 @@ type Props = {
 };
 
 export default function TodoItem({ todo, onToggleComplete, onDelete }: Props) {
+  const [text, setText] = useState(todo.task);
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <TouchableWithoutFeedback>
       <View style={styles.list}>
-        <Text style={styles.text}>{todo.task}</Text>
+        {!isEditing && (
+          <Text style={styles.text} onPress={() => setIsEditing(!isEditing)}>
+            {todo.task}
+          </Text>
+        )}
+        {isEditing && <TextInput value={text} autoFocus onSubmitEditing={() => setIsEditing(!isEditing)} />}
         <Checkbox value={todo.completed} onValueChange={() => onToggleComplete(todo.id)} />
         <Button title="削除" onPress={() => onDelete(todo.id)} />
       </View>
